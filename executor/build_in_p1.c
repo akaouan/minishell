b981b1dd1e8254622247ebaadf399119428eb1c5
akaouan <abdomg133@gmail.com>
2022-07-02 16:22:37 -0400

    finishing echo cmd

--- executor/build_in_p1.c
+++ executor/build_in_p1.c
@@ -40,21 +40,44 @@ void    cmd_pwd(t_prior *data)
     printf("%s\n", data->cmd->env_valuue[i]);
 }
 
-int is_option(char **args)
+int is_option(char *args)
 {
+    int i;
 
-}
-
-int get_args_start(char **args)
-{
-
+    i = 0;
+    if (!args)
+        return (0);
+    if(args[0] == '-')
+    {
+        while (args[++i])
+        {
+            if (args[i] != 'n')
+                return (0);
+        }
+        return (1);
+    }
+    return (0);
 }
 
 void    cmd_echo(int fd_output, char **args)
 {
+    int i;
     int option;
-    int args_start;
 
-    option = is_option(args);
-    args_start = get_args_start(args);
+    i = 0;
+    while (is_option(args[i]))
+    {
+        i++;
+    }
+    option = i;
+    while (1)
+    {
+        ft_putstr_fd(args[i++], fd_output);
+        if (args[i])
+            ft_putchar_fd(' ', fd_output);
+        else
+            break ;
+    }
+    if (!option)
+        ft_putchar_fd('\n', fd_output);
 }
