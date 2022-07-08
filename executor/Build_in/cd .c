d8f4b2d3ad249543f985e0c8ad106b78315ed0e6
akaouan <abdomg133@gmail.com>
2022-07-07 20:58:56 -0400

    finishing pwd and cd cmd

--- executor/Build_in/cd .c
+++ executor/Build_in/cd .c
@@ -1,7 +1,31 @@
 #include "../minishell.h"
 
+int	get_last_char(char *pwd, char c)
+{
+	int i;
+	int	last_char;
+
+	i = -1;
+	last_char = 0;
+	while (pwd[++i])
+		if (pwd[i] == '/')
+			last_char = i;
+	return (last_char);
+}
+
+void	rm_back(char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+		if (str[i] == '/')
+			str[i] = '\0';
+}
+
 void	cmd_cd(char **old_pwd, char **pwd, char *path)
 {
+	rm_back(path);
 	if(chdir(path) == -1)
 	{
 		ft_putstr_fd("cd: no such file or directory: ", STDERR_FILENO);
@@ -9,8 +33,17 @@ void	cmd_cd(char **old_pwd, char **pwd, char *path)
 		ft_putstr_fd("\n", STDERR_FILENO);
 		exit(EXIT_FAILURE);	
 	}
-	path = ft_strjoin(ft_strdup("/"), path);
-	// free(old_pwd);
-	*old_pwd = ft_strdup(*pwd);
-	*pwd = ft_strjoin(*pwd, path);
+	if (!ft_strncmp(path, "..", 2))
+	{
+		free(*old_pwd);
+		*old_pwd = ft_strdup(*pwd);
+		*pwd = ft_substr(*pwd, 0, get_last_char(*pwd, '/'));
+	}
+	else
+	{
+		path = ft_strjoin(ft_strdup("/"), path);
+		free(*old_pwd);
+		*old_pwd = ft_strdup(*pwd);
+		*pwd = ft_strjoin(*pwd, path);
+	}
 }
